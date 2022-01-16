cd2d5ec1c56b2ce479257f2b74d0f5fa55206746
ClaudioLoz <a20180472@pucp.edu.pe>
2022-01-16 01:03:43 -0500

    pruebas

--- FrontEnd/index-copy.js
+++ FrontEnd/index-copy.js
@@ -1,14 +1,49 @@
 var search = document.getElementById("search");
 var button = document.getElementById("button");
 var input = document.getElementById("input");
+let info = undefined;
 const profileURL = "http://127.0.0.1:5500/profile.html";
-function loading() {
+const IPBack =
+  "http://95b7-181-65-18-224.ngrok.io/web-perfil/perfil/crearPerfil";
+
+function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return true;
+}
+
+async function loading() {
+  let url = document.getElementById("input").value;
+  //validations
+  if (url === "") return;
+  if (!isValidHttpUrl(url)) {
+    alert("Ingrese una URL válida");
+    return;
+  }
+
   search.classList.add("loading");
 
-  setTimeout(function () {
-    search.classList.remove("loading");
-    location.assign(profileURL);
-  }, 3000);
+  let objectt = { url: url };
+
+  info = await fetch(IPBack, {
+    method: "POST",
+    body: JSON.stringify(objectt),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  info = await info.json();
+  console.log(info);
+  localStorage.setItem("object", JSON.stringify(info));
+  search.classList.remove("loading");
+
+  location.assign(profileURL);
 }
 
 button.addEventListener("click", loading);
---
+++ FrontEnd/profile.js
@@ -0,0 +1,2 @@
+let info = localStorage.getItem("object");
+console.log(info);
